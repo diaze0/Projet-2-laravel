0720347a8fee132b8f234ae3f008466010239fa8
diaze0 <92938629+diaze0@users.noreply.github.com>
2022-10-25 15:37:12 +0100

    HI

---
+++ Prototype-Premium/resources/views/update_promotion.blade.php
@@ -0,0 +1,8 @@
+
+<form action="/edit/{{ $promotion[0]->id }}" method="get">
+
+    <input type="text" name="name" value="{{ $promotion[0]->name }} ">
+    <button>Edit</button>
+</form>
+
+
---
+++ Prototype-Premium/routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\PromotionController;
+use App\Models\promotion;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/index', [PromotionController::class , 'select']);
+
+Route::get('/add', [PromotionController::class ,'add']);
+
+Route::get('/insert', [PromotionController::class ,'insert']);
+
+Route::get('/delete', [ PromotionController::class,'delete' ]);
+
+Route::get('/update_promotion/{id}', [ PromotionController::class,'update_promotion' ]);
+
+Route::get('/edit/{id}', [ PromotionController::class,'edit']);
+
+
+// Route::post('/edit', [ MainController::class,'edit']);
+
